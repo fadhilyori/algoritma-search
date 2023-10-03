e14dc6377531c07fad7344c902c0deb3fa01048a
Fadhil Yori <me@fadhilyori.my.id>
2023-10-03 13:30:08 +0700

    feat: improve visualization layout

--- visualize.py
+++ visualize.py
@@ -21,6 +21,17 @@ if __name__ == '__main__':
         for edge in current_node:
             graph_net.add_edge(k, edge[0], weight=edge[1])
 
-    nx.draw(graph_net, pos=nx.spring_layout(graph_net), with_labels=True)
+    pos = nx.spring_layout(graph_net, seed=7)
+    edge_labels = nx.get_edge_attributes(graph_net, "weight")
+
+    nx.draw_networkx_nodes(graph_net, pos, node_size=800)
+    nx.draw_networkx_edges(graph_net, pos, width=3)
+    nx.draw_networkx_edge_labels(graph_net, pos, edge_labels)
+    nx.draw_networkx_labels(graph_net, pos, font_size=16, font_family="sans-serif")
+
+    ax = plt.gca()
+    ax.margins(0.08)
+    plt.axis("off")
+    plt.tight_layout()
     plt.show()
 
